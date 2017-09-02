11a96d815e87e82cb2266df1318062a984b4833d
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2017-09-02 07:25:04 -0400

    removed test url

--- index.php
+++ index.php
@@ -8,15 +8,13 @@
     $oldURIs = collect([
         'reseau' => 'https://www.zoneski.com/reseau/',
         'ecole' => 'https://www.zoneski.com/ecole/',
-        'forum' => 'https://www.zoneski.com/forum/',
-        'test' => 'http://zoneski-redirector.dev/',
+        'forum' => 'https://www.zoneski.com/forum/'
     ]);
 
     $newURIs = collect([
         'reseau' => 'https://zone.ski/',
         'ecole' => 'https://boutiqu.zone.ski/',
         'forum' => 'https://forum.zone.ski/',
-        'test' => 'https://zone.ski/',
     ]);
 
     $currentURI = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
