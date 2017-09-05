22fb6483cd47871a8df48b5a76dde9e94abc7004
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2017-09-04 20:03:31 -0400

    debug

--- public/index.php
+++ public/index.php
@@ -32,6 +32,7 @@ De retour dans quelques minutes...
     $oldURIs->each(function ($uri, $key) use ($currentURI, $newURIs) {
         
         $found = strpos($currentURI, $uri);
+        dd($currentURI, $uri, $found);
         
         if ($found !== false) {
             $redirectTo = str_replace($uri, $newURIs[$key], $currentURI);
